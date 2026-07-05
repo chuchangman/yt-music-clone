db7d92a604217b6c0ee273e394857e397c6510c7
추창만 <chuchangman@gmail.com>
2026-07-05 22:09:26 +0900

    second

---
+++ app/(site)/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Category from './components/Category'
+
+const page = () => {
+
+  return (
+    <div className='min-h-[600px]'>
+      <div className='mt-9'></div>
+      <Category/>
+    </div>
+  )
+}
+
+export default page
