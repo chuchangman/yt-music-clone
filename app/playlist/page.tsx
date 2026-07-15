2e7a6e972d48fb4f7fd15db744d4a5c3a5a3e0bd
추창만 <chuchangman@gmail.com>
2026-07-15 17:59:36 +0900

    ssafy3

--- app/playlist/page.tsx
+++ app/playlist/page.tsx
@@ -1,8 +1,30 @@
+import { getPlaylistById } from '@/lib/dummyData';
+import { getRandomElementFromArray } from '@/lib/utils';
+import { permanentRedirect } from 'next/navigation';
 import React from 'react'
+import HeaderBgChanger from '@/components/HeaderBgChanger'
+import PagePadding from '@/components/PagePadding';
+import PlayListHead from '@/components/PlayListHead';
 
-const page = (props) => {
+interface PlaylistPageProps {
+  searchParams:{
+    list:string;
+  }
+}
+
+const page = async (props:PlaylistPageProps) => {
+  const playlist = await getPlaylistById(Number(props.searchParams.list))
+  
+  if(!playlist) permanentRedirect('/')
+
+  const imageSrc = getRandomElementFromArray(playlist.songList)?.imageSrc
+  
   return (
-    <div>playlist {props.searchParams.list}</div>
+    <PagePadding>
+      <HeaderBgChanger imageSrc={imageSrc}/>
+      <div className='mt-12'></div>
+      <PlayListHead playlist={playlist} />
+    </PagePadding>
   )
 }
 
